a27321e233b0c13dfaaa33899349e3686e032f70
Anas <anasjelloule@gmail.com>
2023-04-25 05:23:05 -0700

    _printf.c Count Numbers 3

--- _printf.c
+++ _printf.c
@@ -10,11 +10,31 @@ int _printf(const char *format, ...)
 {
 int i, princahr = 0;
 va_list list;
+char *s;
 if (format == NULL)
 return (-1);
 va_start(list, format);
 for (i = 0; format && format[i] != '\0'; i++)
-if (format[i] != '%')
+if (format[i] == '%')
+{
+i++;
+switch (format[i])
+{
+case '%':
+princahr += _putchar('%');
+break;
+case 'c':
+princahr += _putchar(va_arg(list, int));
+break;
+case 's':
+s = va_arg(list, char *);
+if (!s)
+s = "(null)";
+princahr += _puts(s);
+break;
+}
+}
+else
 princahr += _putchar(format[i]);
 va_end(list);
 return (princahr);
