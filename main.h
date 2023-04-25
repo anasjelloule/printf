1693c6eef1d14a1e68efcebd82742956c5aec93b
Anas <anasjelloule@gmail.com>
2023-04-25 04:17:20 -0700

    _printf.c Find new solution

--- main.h
+++ main.h
@@ -1,5 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
+int _printf(const char *format, ...);
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
 #endif
